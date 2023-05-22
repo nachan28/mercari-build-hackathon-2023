6c0e00730efb8eaa2c486a60f1b9bca26009c09b
nachan28 <torajiro282828@gmail.com>
2023-05-22 10:50:06 +0900

    setup

--- backend/db/repository.go
+++ backend/db/repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/mercari-build/mecari-build-hackathon-2023/backend/domain"
+	"github.com/nachan28/mecari-build-hackathon-2023/backend/domain"
 )
 
 type UserRepository interface {
